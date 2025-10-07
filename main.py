12b0a6e7c394bd58ce9006a62553a8a7275181af
miljyface <pommybeloved.cc@gmail.com>
2025-10-07 11:44:53 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+from methods.shrineoforder import order
+import pandas as pd
+
+hptalents = pd.read_json('data/hptalents.json', typ='series').to_dict()
+
+class build:
+    def __str__(self):
+        return f"{self.name}\n{self.desc}"
+    
+    def __init__(self, build_id):
+        response = requests.get(f'https://api.deepwoken.co/build?id={build_id}')
+        data = response.json()
+        stats = data['stats']
+        self.name = stats['buildName']
+        self.desc = stats['buildDescription']
+        self.talents = data['talents']
+        self.pre = data['preShrine']
+        self.post = data['attributes']
+        self.mantras = data['mantras']
+        self.traits = stats['traits']
+        self.outfit = stats['meta']['Outfit']
+        self.race = stats['meta']['Race']
+        self.flatpre = {}
+        self.flatpost = {}
+        for cat in data['preShrine']:
+            for k, v in data['preShrine'][cat].items():
+                self.flatpre[k] = v
+
+        for cat in data['attributes']:
+            for k, v in data['attributes'][cat].items():
+                self.flatpost[k] = v
+
+        self.health = self.calculate_health(stats, self.traits, self.post['base'], self.talents)
+        
+        self.flags = [
+        10 + 0.8*(self.post['base']['Fortitude'] - 65) if 'Reinforced Armor' in set(self.talents) else 0,
+        5.83 + (self.post['base']['Fortitude']-25)*0.16 if self.post['base']['Fortitude'] < 50 and 'To The Finish' in set(self.talents) else 10 if 'To The Finish' in set(self.talents) else 0,
+        self.post['base']['Charisma']*0.15 if 'Chaotic Charm' in set(self.talents) else 0,
+        1 if self.post['base']['Fortitude'] >= 60 and 'Reinforce' in set(self.mantras) else 0
+        ] 
+        #Reinforced Armor, TTF, CCharm, Reinforce
+    
+    @classmethod
+    def calculate_health(cls, stats, traits, base_attrs, talents):
+        hp = traits['Vitality'] * 10 + 196 + stats['power'] * 4
+
+        fortitude = base_attrs['Fortitude']
+        if fortitude <= 50:
+            hp += fortitude / 2
+        else:
+            hp += (fortitude - 50) / 4 + 25
+
+        hp += sum(hptalents[talent] for talent in set(talents) & hptalents.keys())
+        return hp
+
+    def ehp(self, params = {'dps':100, 'pen':50, 'kithp': 100, 'kitresis':50}):
+        scaledDps = params['dps'] * self.resisCoefficient(params['pen'], 10, 50) if self.flags[3] else params['dps']
+        EHP = (scaledDps * (self.health + params['kithp']))/((scaledDps)*self.resisCoefficient(params['pen'], params['kitresis'], self.flags[0]))
+        EHP *= ((30/(100 - self.flags[1]) + 0.7) if self.flags[1] != 0 else 1) * ((25/(100 - self.flags[2]) + 0.75 if self.flags[2] != 0 else 1))
+        return round(EHP)
+
+    @classmethod
+    def resisCoefficient(cls ,pen ,res ,penres) -> float:
+        return (1- ((res/100) * (1 - (pen * (1 - penres/100) / 100))))
+
+def comparison(id):
+    build1 = build(id)
+    ordered = order(build1.pre.copy(),{"Race":build1.race, "PointsSpent":0})
+    reinvest = 0
+
+    print("\nShrine Process:")
+    for k, v in ordered.items():
+        if ordered[k] != 0:
+            print(f"{k}: {build1.flatpre[k]} -> {v}")
+    print("\nProgression:")
+    for k, v in ordered.items():
+        if ordered[k] != 0:
+            print(f"{k}: {v} -> {build1.flatpost[k]}, Change: {build1.flatpost[k]-v}")
+            if build1.flatpost[k]-v <= 50:
+                reinvest += build1.flatpost[k]-v
+    print(f"\nTotal reinvested points: {reinvest}")
+
+comparison('WORKSHOP')
---
+++ methods/shrineoforder.py
@@ -0,0 +1,110 @@
+import numpy as np
+from collections import defaultdict
+import pandas as pd
+
+racial_stats = pd.read_json('data/racialstats.json', typ='series').to_dict()
+
+attunements = ["Flamecharm", "Frostdraw", "Thundercall", "Galebreathe", "Shadowcast", "Ironsing", "Bloodrend"]
+
+playerStats = {
+    "Race": "None",
+    "PointsSpent": 0,
+}
+
+def merge_stats(*dict_args):
+    result = defaultdict(int)
+    for dictionary in dict_args:
+        for key, value in dictionary.items():
+            result[key] += value
+    return dict(result)
+
+MAXIMUM_REDUCTION = 25
+
+def order(stats, player_stats):
+    # Calculate the points spent so far in the build, excluding racial stats
+
+    stats = merge_stats(stats['weapon'], stats['attunement'],stats['base'])    
+
+    for stat_name, value in stats.items():
+        if stat_name in racial_stats[player_stats["Race"]]:
+            value -= racial_stats[player_stats["Race"]][stat_name]
+        player_stats["PointsSpent"] += value
+
+    # Save the points spent so far as point_start
+    points_start = player_stats["PointsSpent"]
+    
+    # Create a shallow copy of the current stats as pre-shrine
+    preshrine_build = stats.copy()
+    
+    # Identify which stats are invested for the shrine
+    affected_stats = []
+    for stat_name, stat_value in stats.items():
+        if stat_value > 0:
+            if stat_name in racial_stats[player_stats["Race"]]:
+                if racial_stats[player_stats["Race"]][stat_name] > 0:
+                    if stat_value - racial_stats[player_stats["Race"]][stat_name] == 0:
+                        continue  # Skip if stat - racialStat equals to 0 (not invested)
+            affected_stats.append(stat_name)
+                # Initial division of points to every affected stat
+    for stat_name in stats.keys():
+        if stat_name in affected_stats:
+            stats[stat_name] = points_start / len(affected_stats)
+
+    # Prepare for the bottlenecking step
+    bottlenecked = []
+    bottlenecked_divide_by = len(affected_stats)
+    previous_stats = stats.copy()
+
+    while True:
+        bottlenecked_points = 0
+        bottlenecked_stats = False
+
+        for stat_name, stat_value in stats.items():
+            # Only bottleneck non-attunement affected stats
+            if stat_name not in attunements and stat_name in affected_stats:
+                shrine_stat = preshrine_build[stat_name]
+
+                if shrine_stat - stat_value > MAXIMUM_REDUCTION:
+                    # Set the stat to preshrine value - MAXIMUM_REDUCTION
+                    stats[stat_name] = shrine_stat - MAXIMUM_REDUCTION
+                    # Add the difference to bottlenecked points
+                    bottlenecked_points += stats[stat_name] - previous_stats[stat_name]
+                    bottlenecked.append(stat_name)
+                    bottlenecked_divide_by -= 1  # Reduce the division counter
+
+        # Averaging out bottlenecked points
+        for stat_name, stat_value in stats.items():
+            if stat_name in affected_stats and stat_name not in bottlenecked:
+                stats[stat_name] -= bottlenecked_points / bottlenecked_divide_by
+                
+                # Check if any stat reduction is still larger than 25
+                if stat_name not in attunements:
+                    if preshrine_build[stat_name] - stats[stat_name] > 25:
+                        bottlenecked_stats = True
+
+        previous_stats = stats.copy()
+        
+        # Exit loop if no bottlenecked stats
+        if not bottlenecked_stats:
+            break
+
+    # Round down all the stats and refund extra points
+    for stat_name in stats.keys():
+        stats[stat_name] = int(stats[stat_name])  # Rounding down
+
+    # Calculate points spent after shrine
+    points_spent_after_shrine = 0
+    for stat_name, value in stats.items():
+        if stat_name in racial_stats[player_stats["Race"]]:
+            value -= racial_stats[player_stats["Race"]][stat_name]
+        points_spent_after_shrine += value
+
+    # Calculate spare points
+    spare_points = points_start - points_spent_after_shrine
+
+    # Ensure spare points do not exceed the number of affected stats
+    if spare_points > len(affected_stats):
+        for stat_name in affected_stats:
+            stats[stat_name] += 1
+
+    return stats #, spare_points
